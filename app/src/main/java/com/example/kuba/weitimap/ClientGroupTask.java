0b521c669f7b24994915b784f52354ac6c09154d
Mozart <kioladusa@gmail.com>
2016-05-06 02:07:38 +0200

    prefixed message: sent from server and received on android

--- app/src/main/java/com/example/kuba/weitimap/ClientGroupTask.java
+++ app/src/main/java/com/example/kuba/weitimap/ClientGroupTask.java
@@ -3,11 +3,14 @@ package com.example.kuba.weitimap;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 
 /**
  * Created by Kuba on 2016-05-02.
@@ -17,6 +20,14 @@ public class ClientGroupTask implements Runnable {
     final static String TAG = "ClientTaskTAG";
     private Socket mySocket;
     private String groupName;
+
+    private PrintWriter out;
+    private BufferedReader in;
+
+    private ObjectInputStream objIn;
+
+    private DataOutputStream outData;
+    private DataInputStream inData;
 
     ClientGroupTask(Socket socket, String groupname) {
         mySocket = socket;
@@ -45,25 +56,46 @@ public class ClientGroupTask implements Runnable {
     public void run() {
 
             try {
-                PrintWriter out =
-                        new PrintWriter(mySocket.getOutputStream(), true);
-                BufferedReader in =
-                        new BufferedReader(
-                                new InputStreamReader(mySocket.getInputStream()));
-                ObjectInputStream objIn =
-                        new ObjectInputStream(mySocket.getInputStream());
+                out = new PrintWriter(mySocket.getOutputStream(), true);
+                in = new BufferedReader( new InputStreamReader(mySocket.getInputStream()));
 
-                if (handshake(out, in)) {
-                    getGroupPlan(out, in, objIn);
+                outData = new DataOutputStream(mySocket.getOutputStream());
+                inData = new DataInputStream(mySocket.getInputStream());
+
+                objIn = new ObjectInputStream(mySocket.getInputStream());
+
+//                if (handshake(out, in)) {
+//                    getGroupPlan(out, in, objIn);
+//                }
+//
+//                out.print("CLOSE_SOCKET");
+                while (true) {
+                    Log.d(TAG, receivePrefixedMessage());
                 }
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
         Log.d(TAG, "Communication has ended");
     }
 
     private void getGroupPlan(PrintWriter out, BufferedReader in, ObjectInputStream objIn) {
+        String clientInput;
+        String groupRequest = MyAndUtils.GET_GROUP_REQUEST + groupName;
+        out.println(groupRequest);
+        Log.d(TAG, groupRequest + " - request sent");
+        while (true) {
+            try {
+                clientInput = in.readLine();
+                if (clientInput != null) {
+                    Log.d(TAG,"received message: " + clientInput);
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
@@ -95,6 +127,63 @@ public class ClientGroupTask implements Runnable {
         return false;
     }
 
+    private String receivePrefixedMessage() {
+        byte[] prefixBuffer = new byte[4];
+        int prefixBytesToRead = 4;
+        int prefixBytesRead = 0;
+
+        // prefix reading
+        while (prefixBytesToRead > 0) {
+            try {
+                int n = inData.read(prefixBuffer, prefixBytesRead, prefixBytesToRead);
+                if (n == 0) {
+                    return null;
+                }
+                prefixBytesToRead -= n;
+                prefixBytesRead += n;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String hexString = MyAndUtils.bytesToHex(prefixBuffer);
+        Log.d(TAG, "Hex of message's length: " + hexString);
+        // end of prefix reading
+
+        final ByteBuffer b = ByteBuffer.wrap(new String(prefixBuffer).getBytes());
+//        b.order(ByteOrder.BIG_ENDIAN);
+        int dataLength = b.getInt() - 4;
+        Log.d(TAG, "DataLength: " + dataLength);
+
+        // actual message reading
+        int dataBytesToRead = dataLength;
+        int dataBytesRead = 0;
+        // if dataLenght < 0 || > INFINITY ... throw something throwable
+
+        byte[] dataBuffer = new byte[dataLength];
+        while (dataBytesToRead > 0) {
+            int n;
+            try {
+                n = inData.read(dataBuffer, dataBytesRead, dataBytesToRead);
+                Log.d(TAG, "read bytes number: " + n);
+                if (n == 0) return null;
+                dataBytesRead += n;
+                dataBytesToRead -= n;
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        // end of actual message reading
+
+        hexString = MyAndUtils.bytesToHex(dataBuffer);
+        Log.d(TAG, "Hex actual message: " + hexString);
+
+        String ret = new String(dataBuffer);
+        System.out.println(ret);
+        return ret;
+    }
 
 
 }
--- app/src/main/java/com/example/kuba/weitimap/MyAndUtils.java
+++ app/src/main/java/com/example/kuba/weitimap/MyAndUtils.java
@@ -3,10 +3,25 @@ package com.example.kuba.weitimap;
 public class MyAndUtils {
 
     public final static String DOWNLOAD_NAME = "DOWNLOAD_PAGE";
-    public final static String EMAIL_ADDRESS = "jakubkoladadev@gmail.com";
     public final static String ASUS_VANTAGE_DEFAULT_IP = "192.168.0.172";
     public final static String SERVER_DEFAULT_IP = "192.168.0.42";
     public final static String SERVER_DEFAULT_PORT = "13131";
+    public final static String GET_GROUP_REQUEST =  "GET_GROUP:_";
+
+    final static String EMAIL_ADDRESS = "<HANDSHAKE/jakubkoladadev@gmail.com>";
+    final static char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+    final static String GET_GROUP = "<GET_GROUP/";
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
 
 
     public enum parity {
